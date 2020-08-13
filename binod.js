2d1be8a12dfc119d3ce88df1a749274ab434ef83
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2020-08-13 15:24:14 +0530

    Update binod.js

--- binod.js
+++ binod.js
@@ -1,1 +1,9 @@
-console.log("Binod");
+(() => {
+  const name = "Pritam Ghosh";
+
+  console.log(
+    `${(typeof !name).toString().toUpperCase().substring(0, 1)}${
+      (name.length / 0).toString().toLowerCase().split("f")[0]
+    }${name.match(/([oops!])\w+/)[1]}${(typeof name[-1]).toString().slice(-1)}`
+  );
+})();
